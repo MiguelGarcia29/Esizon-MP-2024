36e64707b1be3361581780ce05f0dcdbf689c69b
Miguel <164225282+MiguelGarcia29@users.noreply.github.com>
2024-04-10 19:32:17 +0200

    esto

---
+++ Codigo/Cliente.h
@@ -0,0 +1,10 @@
+//cliente
+#include <stdio.h>
+#include "Proveedor.h"
+#include "Estructuras.h"
+
+
+
+void mostrar_cliente(Cliente cliente);
+
+void cambiar_perfil_cliente(Cliente *cliente); //se presentara un menu para que el cliente pueda cambiar los datos que considere necesario
