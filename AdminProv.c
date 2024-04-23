4c9d0c210e3a6e14b99b2af0e6736d1b527f7bbd
Manuel Gutierrez Recio <162916542+Manuu058@users.noreply.github.com>
2024-04-23 20:08:34 +0200

    Comentado y sin tildes

--- AdminProv.c
+++ AdminProv.c
@@ -1,5 +1,6 @@
 #include "AdminProv.h"
 
+// Función para generar un ID para un administrador o proveedor
 char* id_generator(AdminProv *a, int tamanio_vector) {
     int id_generada = 1;
 
@@ -20,16 +21,15 @@ char* id_generator(AdminProv *a, int tamanio_vector) {
 
     return id;
 }
-
-
+// Función para agregar un nuevo administrador o proveedor
 void alta_AdminProv(AdminProv **l, int* tamanio_vector) {
     AdminProv nuevo_adminprov;
-    // Genera un nuevo ID para el locker
+    // Genera un nuevo ID para el administrador o proveedor
     char *cadena = id_generator(*l, *tamanio_vector);
     strcpy(nuevo_adminprov.id_empresa, cadena);
     free(cadena); // Libera la memoria asignada para el ID generado
 
-    // Solicita al usuario información sobre el nuevo locker
+    // Solicita al usuario información sobre el nuevo administrador o proveedor
     int respuesta;
     printf("\nSeleccione nuevo rol:");
     printf("\n1-Administrador");
@@ -62,32 +62,32 @@ void alta_AdminProv(AdminProv **l, int* tamanio_vector) {
     scanf("%16s", nuevo_adminprov.contrasenia);
     nuevo_adminprov.contrasenia[strcspn(nuevo_adminprov.contrasenia, "\n")] = '\0';
 
-    // Realiza una realocación de memoria para agregar el nuevo locker al arreglo
+    // Realiza una realocación de memoria para agregar el nuevo administrador o proveedor al arreglo
     *l = (AdminProv *)realloc(*l, (*tamanio_vector + 1) * sizeof(AdminProv));
     if (*l == NULL) {
-        printf("Error al asignar memoria para el nuevo locker. \n");
+        printf("Error al asignar memoria para el nuevo administrador o proveedor. \n");
         exit(EXIT_FAILURE);
     }
-    (*l)[*tamanio_vector] = nuevo_adminprov; // Guarda el nuevo locker en el arreglo
+    (*l)[*tamanio_vector] = nuevo_adminprov; // Guarda el nuevo administrador o proveedor en el arreglo
     (*tamanio_vector)++;
 }
-
+// Función para dar de baja un administrador o proveedor
 void baja_adminprov(AdminProv *l, int* tamanio, char *id_baja) {
     int encontrado = 0;
 
-    // Busca el locker con el ID dado y lo elimina del arreglo
+    // Busca el administrador o proveedor con el ID dado y lo elimina del arreglo
     for (int i = 0; i < *tamanio; i++) {
         if (strcmp(l[i].id_empresa, id_baja) == 0) {
             encontrado = 1;
 
-            // Reorganiza el arreglo para eliminar el locker encontrado
+            // Reorganiza el arreglo para eliminar el administrador o proveedor encontrado
             for (int j = i; j < *tamanio - 1; j++) {
                 l[j] = l[j + 1];
             }
 
             (*tamanio)--; // Reduce el tamaño del arreglo
             printf("Ha sido dado de baja correctamente.\n", id_baja);
-            break; // Sale del bucle una vez encontrado y eliminado el locker
+            break; // Sale del bucle una vez encontrado y eliminado el administrador o proveedor
         }
     }
 
@@ -95,14 +95,13 @@ void baja_adminprov(AdminProv *l, int* tamanio, char *id_baja) {
         printf("La ID registrada no ha sido encontrada.\n");
     }
 }
-
+// Función para listar los administradores o proveedores
 void listado_adminprov(AdminProv *l, int tamanio) {
-    // Recorre el arreglo de lockers y muestra la información de cada uno
+    // Recorre el arreglo de administradores o proveedores y muestra la información de cada uno
     for (int i = 0; i < tamanio; i++) {
         printf("%s-%s-%s-%s-%s\n", l[i].id_empresa, l[i].nombre, l[i].email, l[i].contrasenia, l[i].perfil_usuario);
     }
 }
-
 // Guarda el vector de AdminYProvs en el archivo siguiendo la estructura:
 /*
     o Identificador de la empresa administradora o proveedora (Id_empresa), 4 digitos.
@@ -133,19 +132,19 @@ void guardarAdminProvEnArchivo(AdminProv *admins, int numAdmins)
 
     fclose(archivo);
 }
-
+// Función para iniciar el vector de administradores o proveedores desde un archivo
 AdminProv *iniciarAdminProvDeArchivo(int *numAdmins)
 {
-    FILE *archivo = fopen(AdminProv_txt, "r");
+    FILE *archivo = fopen("AdminProv.txt", "r");
     if (archivo == NULL)
     {
-        printf("Error al abrir el archivo %s.\n", AdminProv_txt);
+        printf("Error al abrir el archivo AdminProv.txt.\n");
         return NULL;
     }
 
-    // Contar la cantidad de lineas en el archivo
+    // Contar la cantidad de líneas en el archivo
     int count = 0;
-    char buffer[TAMANIO_MAXIMO_LINEA]; // Longitud maxima de linea
+    char buffer[TAMANIO_MAXIMO_LINEA]; // Longitud máxima de línea
     while (fgets(buffer, TAMANIO_MAXIMO_LINEA, archivo) != NULL)
     {
         count++;
@@ -154,7 +153,7 @@ AdminProv *iniciarAdminProvDeArchivo(int *numAdmins)
     // Regresar al inicio del archivo
     rewind(archivo);
 
-    // Crear el vector de adminProv
+    // Crear el vector de administradores o proveedores
     AdminProv *adminProvs = (AdminProv *) malloc(count * sizeof(AdminProv));
     if (adminProvs == NULL)
     {
@@ -163,21 +162,21 @@ AdminProv *iniciarAdminProvDeArchivo(int *numAdmins)
         return NULL;
     }
 
-    // Leo cada linea y rellenar el vector de adminProv
-    for(int i=0;i<count;i++)
+    // Leer cada línea y rellenar el vector de administradores o proveedores
+    for(int i=0; i < count; i++)
     {
-        if(fgets(buffer, TAMANIO_MAXIMO_LINEA, archivo) != NULL){
-        char *token = strtok(buffer, "-");
-        strncpy(adminProvs[i].id_empresa, token, sizeof(adminProvs[i].id_empresa));
-        token = strtok(NULL, "-");
-        strncpy(adminProvs[i].nombre, token, sizeof(adminProvs[i].nombre));
-        token = strtok(NULL, "-");
-        strncpy(adminProvs[i].email, token, sizeof(adminProvs[i].email));
-        token = strtok(NULL, "-");
-        strncpy(adminProvs[i].contrasenia, token, sizeof(adminProvs[i].contrasenia));
-        token = strtok(NULL, "-");
-        strncpy(adminProvs[i].perfil_usuario, token, sizeof(adminProvs[i].perfil_usuario));
-
+        if(fgets(buffer, TAMANIO_MAXIMO_LINEA, archivo) != NULL)
+        {
+            char *token = strtok(buffer, "-");
+            strncpy(adminProvs[i].id_empresa, token, sizeof(adminProvs[i].id_empresa));
+            token = strtok(NULL, "-");
+            strncpy(adminProvs[i].nombre, token, sizeof(adminProvs[i].nombre));
+            token = strtok(NULL, "-");
+            strncpy(adminProvs[i].email, token, sizeof(adminProvs[i].email));
+            token = strtok(NULL, "-");
+            strncpy(adminProvs[i].contrasenia, token, sizeof(adminProvs[i].contrasenia));
+            token = strtok(NULL, "-");
+            strncpy(adminProvs[i].perfil_usuario, token, sizeof(adminProvs[i].perfil_usuario));
         }
     }
 
