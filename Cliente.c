e09cee6e2c59ff6ef807937989ae5943c38d3700
Manuel Gutierrez Recio <162916542+Manuu058@users.noreply.github.com>
2024-04-23 20:16:06 +0200

    Comentado y sin tildes

--- Cliente.c
+++ Cliente.c
@@ -1,179 +1,134 @@
-//Cliente
-
 #include "cliente.h"
 
-
-
+// Funci贸n para mostrar los detalles de un cliente
 void mostrar_cliente(Cliente client)
 {
-
-	printf("\nHola %s\n",client.nomb_cliente);
-	printf("Direccion: %s, %s, %s\n",client.dir_cliente,client.localidad,client.provincia);
-	printf("Email: %s\n",client.email);
-	printf("Saldo: %.2f\n",client.cartera);
-	printf("----------\n\n");
-
+    printf("\nHola %s\n",client.nomb_cliente);
+    printf("Direccion: %s, %s, %s\n",client.dir_cliente,client.localidad,client.provincia);
+    printf("Email: %s\n",client.email);
+    printf("Saldo: %.2f\n",client.cartera);
+    printf("----------\n\n");
 }
 
-
-void cambiar_perfil_cliente(Cliente *client) //se presentara un menu para que el cliente pueda cambiar los datos que considere necesario
+// Funci贸n para que un cliente cambie su perfil
+void cambiar_perfil_cliente(Cliente *client)
 {
-
-	int a;
-
-	printf("(1) Cambiar direccion\n");
-	printf("(2) Cambiar email\n");
-	printf("(3) Cambiar contrasenia\n");
-	printf("(4) Acceder a mi cartera\n");
-
-	scanf("%d",&a);
-	printf("----------\n\n");
-
-	switch(a)
-	{
-		case(1):
-			cambiar_direccion_cliente(client->dir_cliente, client->provincia, client->localidad); //funcion cambiar direccion(provincia y localidad incluida)
-		break;
-
-		case (2):
-			cambiar_email_cliente(client->email); //funcion cambiar email
-		break;
-
-		case (3):
-			cambiar_contrasenia_cliente(client->contrasenia); //funcion cambiar contrasenia
-		break;
-
-		case (4):
-			cartera(&client->cartera); //acceder a la cartera para retirar o aniadir dinero
-		break;
-
-		default:
-			printf("Error\n\n");
-		break;
-	}
-
+    int a;
+    printf("(1) Cambiar direccion\n");
+    printf("(2) Cambiar email\n");
+    printf("(3) Cambiar contrasenia\n");
+    printf("(4) Acceder a mi cartera\n");
+    scanf("%d",&a);
+    printf("----------\n\n");
+    switch(a)
+    {
+        case(1):
+            cambiar_direccion_cliente(client->dir_cliente, client->provincia, client->localidad);
+        break;
+        case (2):
+            cambiar_email_cliente(client->email);
+        break;
+        case (3):
+            cambiar_contrasenia_cliente(client->contrasenia);
+        break;
+        case (4):
+            cartera(&client->cartera);
+        break;
+        default:
+            printf("Error\n\n");
+        break;
+    }
 }
 
-
-void cambiar_contrasenia_cliente(char nueva[16]) // funcion que cambia la contrasenia
+// Funci贸n para cambiar la contrase帽a de un cliente
+void cambiar_contrasenia_cliente(char nueva[16])
 {
-	printf("Introduzca su nueva contrasenia: ");
-	fgets(nueva,16,stdin);
-	nueva[strcspn(nueva, "\n")] = '\0';
+    printf("Introduzca su nueva contrasenia: ");
+    fgets(nueva,16,stdin);
+    nueva[strcspn(nueva, "\n")] = '\0';
 }
 
-void cambiar_email_cliente(char nuevo[31]) //funcion que cambia el email
+// Funci贸n para cambiar el email de un cliente
+void cambiar_email_cliente(char nuevo[31])
 {
-	printf("Introduzca el nuevo email: ");
-	fgets(nuevo,31,stdin);
-	nuevo[strcspn(nuevo, "\n")] = '\0';
+    printf("Introduzca el nuevo email: ");
+    fgets(nuevo,31,stdin);
+    nuevo[strcspn(nuevo, "\n")] = '\0';
 }
 
-void cambiar_direccion_cliente(char direccion[51], char provincia[21], char localidad[21]) //funcion que cambia direccion, provincia y localidad
+// Funci贸n para cambiar la direcci贸n de un cliente
+void cambiar_direccion_cliente(char direccion[51], char provincia[21], char localidad[21])
 {
-
-	printf("Provincia: ");
-	fgets(provincia,21,stdin);
+    printf("Provincia: ");
+    fgets(provincia,21,stdin);
     provincia[strcspn(provincia, "\n")] = '\0';
-
-	printf("Localidad: ");
-	fgets(localidad,21,stdin);
-	localidad[strcspn(localidad,"\n")] = '\0';
-
-
-	printf("Direccion: ");
-	fgets(direccion,51,stdin);
-	direccion[strcspn(direccion,"\n")] = '\0';
-
+    printf("Localidad: ");
+    fgets(localidad,21,stdin);
+    localidad[strcspn(localidad,"\n")] = '\0';
+    printf("Direccion: ");
+    fgets(direccion,51,stdin);
+    direccion[strcspn(direccion,"\n")] = '\0';
 }
 
-
-int corroborar_contrasenia(char cad[16])// para modificar los datos del usuario se pedira la contrasenia
+// Funci贸n para verificar la contrase帽a de un cliente
+int corroborar_contrasenia(char cad[16])
 {
-	char verificacion[16];
-	int a;
-
-	printf("Contrasenia: ");
-	fgets(verificacion,16,stdin);// Se introduce la contrasenia
+    char verificacion[16];
+    int a;
+    printf("Contrasenia: ");
+    fgets(verificacion,16,stdin);
     verificacion[strcspn(verificacion, "\n")] = '\0';
-
-	a = strcmp(verificacion,cad);
-
-	return a; //si devuelve 0 el usuario ha introducido la contrasenia correcta
+    a = strcmp(verificacion,cad);
+    return a;
 }
 
-
+// Funci贸n para manejar la cartera de un cliente
 void cartera(float *saldo)
 {
-	int a;
-
-	printf("(1) Anadir saldo\n");
-	printf("(2) Extraer saldo\n");
-
-	scanf("%d",&a);
-
-	switch(a)
-	{
-		case 1:
-			anadir_saldo(saldo);
-		break;
-
-		case 2:
-			extraer_saldo(saldo);
-		break;
-
-		default:
-			printf("Error");
-		break;
-	}
-
+    int a;
+    printf("(1) Anadir saldo\n");
+    printf("(2) Extraer saldo\n");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+            anadir_saldo(saldo);
+        break;
+        case 2:
+            extraer_saldo(saldo);
+        break;
+        default:
+            printf("Error");
+        break;
+    }
 }
 
+// Funci贸n para a帽adir saldo a la cartera de un cliente
 void anadir_saldo(float *saldo)
 {
-	float a;
-
-	printf("Cantidad de dinero a anadir: ");
-
-	scanf("%f",&a);
-
-	*saldo = *saldo + a;
-
+    float a;
+    printf("Cantidad de dinero a anadir: ");
+    scanf("%f",&a);
+    *saldo = *saldo + a;
 }
 
-
+// Funci贸n para extraer saldo de la cartera de un cliente
 void extraer_saldo(float *saldo)
 {
-	float a;
-
-	printf("Cantidad de dinero a extraer: ");
-
-	scanf("%f",&a);
-
-	if (*saldo - a <= 0)
-	{
-		printf("No hay saldo suficiente\n");
-	}
-
-	else
-	{
-		*saldo = *saldo - a;
-	}
+    float a;
+    printf("Cantidad de dinero a extraer: ");
+    scanf("%f",&a);
+    if (*saldo - a <= 0)
+    {
+        printf("No hay saldo suficiente\n");
+    }
+    else
+    {
+        *saldo = *saldo - a;
+    }
 }
 
-// Guarda el vector de clientes en el archivo siguiendo la estructura:
-/*  o Identificador del cliente (Id_cliente), 7 digitos.
-    o Nombre completo del cliente (Nomb_cliente), 20 caracteres maximo.
-    o Direcci髇 del cliente (Dir_cliente), 50 caracteres maximo.
-    o Poblaci髇 (Localidad), 20 caracteres maximo.
-    o Provincia (Provincia), 20 caracteres maximo.
-    o Email (email), 30 caracteres maximo, sera usado como nombre de usuario para el acceso a la
-    plataforma.
-    o Contrasenia para acceder al sistema (Contrasenia), con 15 caracteres maximo.
-    o Cartera (Cartera), almacenara el dinero del que dispone el cliente para poder adquirir los
-    productos.
-    */
-
+// Funci贸n para guardar los clientes en un archivo
 void guardarClientesEnArchivo(Cliente *clientes, int numClientes)
 {
     FILE *archivo = fopen(Clientes_txt, "w");
@@ -199,7 +154,7 @@ void guardarClientesEnArchivo(Cliente *clientes, int numClientes)
     fclose(archivo);
 }
 
-// LE PASO COMO PUNTERO EL NUM DE CLIENTES PARA LUEGO PODER ALMACENARLO EN EL PROGRAMA (MAIN) -> MISMA LOGICA PARA LAS DEMAS LECTURAS
+// Funci贸n para iniciar los clientes desde un archivo
 Cliente *iniciarClientesDeArchivo(int *numClientes)
 {
     FILE *archivo = fopen(Clientes_txt, "r");
@@ -209,7 +164,6 @@ Cliente *iniciarClientesDeArchivo(int *numClientes)
         return NULL;
     }
 
-    // Contar la cantidad de lineas = clientes en el archivo
     int count = 0;
     char buffer[TAMANIO_MAXIMO_LINEA];
     while (fgets(buffer, TAMANIO_MAXIMO_LINEA, archivo) != NULL)
@@ -217,10 +171,8 @@ Cliente *iniciarClientesDeArchivo(int *numClientes)
         count++;
     }
 
-    // Regresar al inicio del archivo
     rewind(archivo);
 
-    // Crear vector cliente
     Cliente *clientes = (Cliente *)malloc(count * sizeof(Cliente));
     if (clientes == NULL)
     {
@@ -229,14 +181,10 @@ Cliente *iniciarClientesDeArchivo(int *numClientes)
         return NULL;
     }
 
-    // Leer cada linea del archivo y rellenar el vector de clientes
     int i = 0;
-    for(int i=0;i<count;i++)
+    for(i=0;i<count;i++)
     {
         if(fgets(buffer, TAMANIO_MAXIMO_LINEA, archivo) != NULL){
-        // Strtok sirve para dividir la cadena en subcadenas por "-"
-        // - OS ADJUNTO DOCUMENTACION CONSULTADA: https://www.ibm.com/docs/es/i/7.5?topic=functions-strtok-tokenize-string
-        // - VIDEO QUE LO EXPLICA CON SUMA: https://www.youtube.com/watch?v=jHdbmHCVQ2c
         char *token = strtok(buffer, "-");
         strncpy(clientes[i].id_cliente, token, sizeof(clientes[i].id_cliente));
         token = strtok(NULL, "-");
@@ -252,7 +200,7 @@ Cliente *iniciarClientesDeArchivo(int *numClientes)
         token = strtok(NULL, "-");
         strncpy(clientes[i].contrasenia, token, sizeof(clientes[i].contrasenia));
         token = strtok(NULL, "-");
-        clientes[i].cartera = atof(token); // atof convierte la cadena en Float.
+        clientes[i].cartera = atof(token);
         }
     }
 
@@ -261,13 +209,12 @@ Cliente *iniciarClientesDeArchivo(int *numClientes)
     return clientes;
 }
 
+// Funci贸n para generar un ID para un cliente
 char *id_generator_cl(Cliente **clientes, int *tamanio_vector)
 {
-
     int id_generada = 1;
-
     if (*tamanio_vector != 0)
-    { // Si no hay nada en el vector, la primer id es 0000011.
+    {
         id_generada = atoi((*clientes)[*tamanio_vector - 1].id_cliente);
         id_generada++;
     }
@@ -277,11 +224,11 @@ char *id_generator_cl(Cliente **clientes, int *tamanio_vector)
         printf("Error al guardar memoria\n");
         exit(EXIT_FAILURE);
     }
-
     sprintf(id, "%07d", id_generada);
     return id;
 }
 
+// Funci贸n para dar de alta un nuevo cliente
 void altaCliente(Cliente **clientes, int *numClientes) {
     Cliente nuevoCliente;
     strcpy(nuevoCliente.id_cliente, id_generator_cl(clientes, numClientes));
@@ -290,7 +237,7 @@ void altaCliente(Cliente **clientes, int *numClientes) {
     fgets(nuevoCliente.nomb_cliente, 21, stdin);
     nuevoCliente.nomb_cliente[strcspn(nuevoCliente.nomb_cliente, "\n")] = '\0';
 
-    printf("Ingrese la direcci髇 del cliente: ");
+    printf("Ingrese la direcci贸n del cliente: ");
     fgets(nuevoCliente.dir_cliente, 51, stdin);
     nuevoCliente.dir_cliente[strcspn(nuevoCliente.dir_cliente, "\n")] = '\0';
 
@@ -306,7 +253,7 @@ void altaCliente(Cliente **clientes, int *numClientes) {
     fgets(nuevoCliente.email, 31, stdin);
     nuevoCliente.email[strcspn(nuevoCliente.email, "\n")] = '\0';
 
-    printf("Ingrese la contrase馻 del cliente: ");
+    printf("Ingrese la contrase帽a del cliente: ");
     fgets(nuevoCliente.contrasenia, 16, stdin);
     nuevoCliente.contrasenia[strcspn(nuevoCliente.contrasenia, "\n")] = '\0';
 
@@ -324,6 +271,7 @@ void altaCliente(Cliente **clientes, int *numClientes) {
     (*numClientes)++;
 }
 
+// Funci贸n para dar de baja un cliente
 void bajaCliente(Cliente **clientes, int *numClientes) {
     char id[8];
     int encontrado = 0;
@@ -333,11 +281,9 @@ void bajaCliente(Cliente **clientes, int *numClientes) {
     for (int i = 0; i < *numClientes; i++) {
         if (strcmp((*clientes)[i].id_cliente, id) == 0) {
             encontrado = 1;
-            // Eliminar el cliente moviendo los elementos hacia adelante
             for (int j = i; j < *numClientes - 1; j++) {
                 (*clientes)[j] = (*clientes)[j + 1];
             }
-            // Reducir el tama駉 del array
             *clientes = (Cliente *)realloc(*clientes, ((*numClientes) - 1) * sizeof(Cliente));
             if (*clientes == NULL) {
                 printf("Error al liberar memoria.\n");
@@ -353,6 +299,7 @@ void bajaCliente(Cliente **clientes, int *numClientes) {
     }
 }
 
+// Funci贸n para buscar un cliente por ID
 void buscarCliente(Cliente **clientes, int *numClientes) {
     char id[8];
     int encontrado = 0;
@@ -363,7 +310,7 @@ void buscarCliente(Cliente **clientes, int *numClientes) {
         if (strcmp((*clientes)[i].id_cliente, id) == 0) {
             encontrado = 1;
             printf("Cliente encontrado:\n");
-            printf("ID: %s, Nombre: %s, Direcci髇: %s, Localidad: %s, Provincia: %s, Email: %s, Cartera: %.2f\n",
+            printf("ID: %s, Nombre: %s, Direcci贸n: %s, Localidad: %s, Provincia: %s, Email: %s, Cartera: %.2f\n",
                    (*clientes)[i].id_cliente, (*clientes)[i].nomb_cliente, (*clientes)[i].dir_cliente,
                    (*clientes)[i].localidad, (*clientes)[i].provincia, (*clientes)[i].email, (*clientes)[i].cartera);
             break;
@@ -374,15 +321,17 @@ void buscarCliente(Cliente **clientes, int *numClientes) {
     }
 }
 
+// Funci贸n para listar todos los clientes
 void listarClientes(Cliente **clientes, int *numClientes) {
     printf("Listado de clientes:\n");
     for (int i = 0; i < *numClientes; i++) {
-        printf("ID: %s, Nombre: %s, Direcci髇: %s, Localidad: %s, Provincia: %s, Email: %s, Cartera: %.2f\n",
+        printf("ID: %s, Nombre: %s, Direcci贸n: %s, Localidad: %s, Provincia: %s, Email: %s, Cartera: %.2f\n",
                (*clientes)[i].id_cliente, (*clientes)[i].nomb_cliente, (*clientes)[i].dir_cliente,
                (*clientes)[i].localidad, (*clientes)[i].provincia, (*clientes)[i].email, (*clientes)[i].cartera);
     }
 }
 
+// Funci贸n para modificar los datos de un cliente
 void modificarCliente(Cliente **clientes, int *numClientes) {
     char id[8];
     int encontrado = 0;
@@ -400,6 +349,3 @@ void modificarCliente(Cliente **clientes, int *numClientes) {
         printf("Cliente no encontrado.\n");
     }
 }
-
-
-
